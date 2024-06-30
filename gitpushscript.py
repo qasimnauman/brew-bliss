b59575298a887621cec24d7fab05ea1208f56db1
qasimnauman <m.qasimnauman@gmail.com>
2024-06-30 21:01:18 +0500

    Updated the gitpushscript.py file

--- gitpushscript.py
+++ gitpushscript.py
@@ -3,12 +3,15 @@ import subprocess
 commitmessage = input("Enter commit message : ")
 branchname = subprocess.run(['git', 'branch', '--show-current'], stdout=subprocess.PIPE)
 branchname = branchname.stdout.decode().strip()
-print(branchname)
 
 addcmd = ['git', 'add', '.']
 commitcmd = ['git', 'commit', '-m', commitmessage]
 pushcmd = ['git', 'push', 'origin', branchname]
 
-subprocess.run(addcmd)
-subprocess.run(commitcmd)
-subprocess.run(pushcmd)
+commands = [addcmd, commitcmd, pushcmd]
+
+def executecommands(commands):
+    for cmd in commands:
+        subprocess.run(cmd)
+
+executecommands(commands)
